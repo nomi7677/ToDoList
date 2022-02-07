a125b699e75e34651ce2320aa7edfdc3e04101c6
Nauman Khalid <wp061005@gmail.com>
2022-02-07 12:23:36 -0500

    To Do List App using Hooks in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import ToDoLists from "./Components/ToDoLists";
 
 function App() {
+  const [inputList, setInputList] = useState(" ");
+  const [items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const listOfItems = () => {
+    setItems((oldArray) => {
+      return [...oldArray, inputList];
+    });
+    setInputList("");
+  };
+  const deleteItems = (id) =>{
+setItems((oldArray)=>{
+  return oldArray.filter((arrElem, index)=>{
+return index !== id;
+  })
+})
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main_div'>
+      <div className='center_div'>
+        <br />
+        <h1> ToDo List</h1>
+        <br />
+        <input
+          type='text'
+          placeholder='Add items here'
+          value={inputList}
+          onChange={itemEvent}
+        />
+        <button onClick={listOfItems}> + </button>
+
+        <ol>
+          {items.map((itemVal, index) => {
+         return   <ToDoLists text={itemVal} 
+         key={index}
+         id={index}
+         onSelect={deleteItems}
+         />
+          })}
+        </ol>
+      </div>
     </div>
   );
 }
